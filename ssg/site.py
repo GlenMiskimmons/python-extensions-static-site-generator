1465941d8daeea85df6c11d0c83a370f4db9ea81
GlenMiskimmons <42587792+GlenMiskimmons@users.noreply.github.com>
2021-01-26 20:48:13 +0000

    Module 04 Menu Extension
    Add Generated Menu to ReStructuredText Parser

--- ssg/site.py
+++ ssg/site.py
@@ -1,6 +1,6 @@
 import sys
 from pathlib import Path
-from ssg import extensions
+from ssg import extensions, hooks
 
 class Site:
     def __init__(self, source, dest, parsers=None):
@@ -29,6 +29,7 @@ class Site:
     def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
         extensions.load_bundled()
+        hooks.event("collect_files", self.source, self.parsers)
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
